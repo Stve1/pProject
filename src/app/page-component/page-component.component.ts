de1d7afb39a52540dcabbc5ae3d971b0347f9f0b
Stve1 <hsvelezc2004@gmail.com>
2023-10-21 23:29:20 -0500

    Diseño plantilla OK

--- src/app/page-component/page-component.component.ts
+++ src/app/page-component/page-component.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-component',
   templateUrl: './page-component.component.html',
   styleUrls: ['./page-component.component.css']
 })
-export class PageComponentComponent {
+export class PageComponentComponent implements OnInit{
 
+  duracion: any;
+  peliculas: any;
+  ngOnInit(): void {
+    console.log(this.duracion)
+    this.duracion = 1;
+    this.peliculas = [
+      'Combo dinosaurios',
+      'Combo Anabelle',
+      'Combo tercero',
+      'Combo cuatro'
+    ]
+  }
+
+  constructor(){
+  }
 }
